a41d795b640f1f94f6139f08893a1daf51c5e006
arkariz <muhammadrisky1401@gmail.com>
2025-06-14 14:57:44 +0700

    chores: export data class

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,3 +1,10 @@
 library;
 
 export 'di/data_module.dart';
+export 'api/repository/movie_repository.dart';
+export 'api/model/movie_detail.dart';
+export 'api/model/movies.dart';
+export 'api/model/movie.dart';
+export 'api/query/favorite_query.dart';
+export 'api/query/get_favorite_query.dart';
+export 'api/query/movie_query.dart';
