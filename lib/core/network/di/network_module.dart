b600a618f086d10a1afa790b9ecba70e682aa510
arkariz <muhammadrisky1401@gmail.com>
2025-06-14 12:37:21 +0700

    chores: replace loggin_interceptor to dio_request_inspector

--- lib/core/network/di/network_module.dart
+++ lib/core/network/di/network_module.dart
@@ -1,8 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:dio_request_inspector/dio_request_inspector.dart';
 import 'package:fithub_home_test/core/network/dio/dio_service.dart';
 import 'package:fithub_home_test/core/network/flavor/network_flavor.dart';
 import 'package:get_it/get_it.dart';
 
 void registerDioService(NetworkFlavor flavor) {
-  GetIt.I.registerLazySingleton<Dio>(() => DioService().createService(flavor));
+  GetIt.I
+    ..registerLazySingleton<DioRequestInspector>(() => DioRequestInspector(isInspectorEnabled: true))
+    ..registerLazySingleton<Dio>(() => DioService().createService(flavor));
 }
--- lib/core/network/dio/dio_service.dart
+++ lib/core/network/dio/dio_service.dart
@@ -1,10 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:dio_request_inspector/dio_request_inspector.dart';
 import 'package:fithub_home_test/core/network/constant/auth_header.dart';
 import 'package:fithub_home_test/core/network/dio/dio_builder.dart';
 import 'package:fithub_home_test/core/network/flavor/network_flavor.dart';
 import 'package:fithub_home_test/core/network/interceptors/auth_header_interceptor.dart';
 import 'package:fithub_home_test/core/network/interceptors/expired_token_interceptor.dart';
-import 'package:fithub_home_test/core/network/interceptors/logging_inteceptor.dart';
+import 'package:get_it/get_it.dart';
 
 class DioService {
   DioService();
@@ -18,7 +19,7 @@ class DioService {
         .addHeaders(AuthHeader.requiredAuthorization)
         .addInterceptor(ExpiredTokenInterceptor())
         .addInterceptor(AuthHeaderInterceptor())
-        .addInterceptor(LoggingInterceptor())
+        .addInterceptor(GetIt.I<DioRequestInspector>().getDioRequestInterceptor())
         .build();
   }
 }
